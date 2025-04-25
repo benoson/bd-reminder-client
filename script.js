665ff8ef3cc033c724831f12adf653ccb79c46a9
benoson <benoson33@gmail.com>
2025-04-25 17:16:27 +0300

    w

---
+++ script.js
@@ -0,0 +1,65 @@
+(async () => {
+  try {
+    const response = await fetch("http://localhost:3015/");
+    const responseJson = await response.json();
+
+    const personsTable = document.getElementById("tbody");
+
+    if (!responseJson?.success || !responseJson?.data || !responseJson.data.length) return;
+
+    responseJson.data.forEach((person) => {
+      const singlePersonRow = document.createElement("tr");
+
+      const nameCell = document.createElement("td");
+      const birthdayCell = document.createElement("td");
+      const promptCell = document.createElement("td");
+
+      [nameCell, birthdayCell, promptCell].forEach((cellEl) => {
+        cellEl.style.border = "1px solid lightslategrey";
+        cellEl.style.borderCollapse = "collapse";
+        cellEl.style.padding = "8px";
+      });
+
+      nameCell.innerText = person.name;
+      birthdayCell.innerText = person.birthday;
+      promptCell.innerText = person.prompt;
+
+      singlePersonRow.appendChild(nameCell);
+      singlePersonRow.appendChild(birthdayCell);
+      singlePersonRow.appendChild(promptCell);
+
+      personsTable.appendChild(singlePersonRow);
+    });
+  } catch (e) {}
+})();
+
+const submit = async () => {
+  const name = document.getElementById("name");
+  const day = document.getElementById("day");
+  const month = document.getElementById("month");
+  const year = document.getElementById("year");
+
+  const prompt = document.getElementById("chat-gpt-prompt");
+
+  try {
+    const response = await fetch("http://localhost:3015/birthday", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt.value,
+        day: day.value,
+        month: month.value,
+        year: year.value,
+        name: name.value,
+      }),
+    });
+
+    const responseJson = await response.json();
+
+    if (responseJson.message) {
+      alert(responseJson.message);
+    }
+  } catch (e) {}
+};
